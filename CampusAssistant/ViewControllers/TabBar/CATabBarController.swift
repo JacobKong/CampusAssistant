25b05a015dfb60583137e2feac78555a21876e09
JacobKong <947124563@qq.com>
2016-02-14 13:12:12 +0800

    Add project.

---
+++ CampusAssistant/ViewControllers/TabBar/CATabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  CATabBarController.swift
+//  CampusAssistant-iOS
+//
+//  Created by JacobKong on 16/2/13.
+//  Copyright © 2016年 com.jacob. All rights reserved.
+//
+
+import UIKit
+import RDVTabBarController
+
+class CATabBarController: RDVTabBarController {
+    
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+def pods
+    pod 'RDVTabBarController'
+end
+
+target 'CampusAssistant' do
+	use_frameworks!
+    pods
+end
+
