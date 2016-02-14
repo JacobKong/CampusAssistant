efcce79ef97fa809957846c81ed208505b1bb8cf
JacobKong <947124563@qq.com>
2016-02-14 12:33:17 +0800

    change .gitignore

---
+++ CampusAssistant/TestVc.swift
@@ -0,0 +1,13 @@
+//
+//  TestVc.swift
+//  CampusAssistant
+//
+//  Created by JacobKong on 16/2/14.
+//  Copyright © 2016年 com.jacob. All rights reserved.
+//
+
+import Cocoa
+
+class TestVc: RDVTabBarController {
+
+}
